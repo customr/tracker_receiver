c628500549ce1cef02e0260b466e57bfc0927cbf
Max <32608693+customr@users.noreply.github.com>
2020-07-08 13:34:29 +0400

    logging improvements

--- src/logs/log_config.py
+++ src/logs/log_config.py
@@ -2,14 +2,19 @@ import logging
 import sys
 from logging.handlers import RotatingFileHandler
 
-PATH = "tracker_receiver/src/logs/history.log"
-LEVEL = logging.INFO
+PATH = "tracker_receiver/src/logs/"
+LEVEL = logging.DEBUG
 frt = logging.Formatter('%(levelname)s :: %(message)s %(asctime)s\n'+'-'*15)
 
 logger = logging.getLogger()
 logger.setLevel(LEVEL)
 
-handler = RotatingFileHandler(PATH, mode='a', maxBytes=10*1024*1024)
+handler = RotatingFileHandler(PATH+'history.log', mode='a', maxBytes=10*1024*1024)
+handler.setLevel(logging.INFO)
+handler.setFormatter(frt)
+logger.addHandler(handler)
+
+handler = RotatingFileHandler(PATH+'debug.log', mode='a', maxBytes=10*1024*1024)
 handler.setLevel(logging.DEBUG)
 handler.setFormatter(frt)
 logger.addHandler(handler)
